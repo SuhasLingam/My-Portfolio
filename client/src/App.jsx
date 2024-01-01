ad5991ab110495ee44d037cfbf231e968f778418
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-01-01 12:19:32 +0530

    FC

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="h-screen w-screen">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
