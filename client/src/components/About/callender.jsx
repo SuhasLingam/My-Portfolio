80ad44891a2fd355e345cc38f6de75516a4f6b0b
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-01-08 02:53:45 +0530

    Made  Icons Responsive and Added Colur to Calendar

--- client/src/components/About/callender.jsx
+++ client/src/components/About/callender.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactGitHubCalendar from "react-github-calendar";
 
 const callender = () => {
+  const explicitTheme = {
+    dark: ["#383838", "#2463FF", "#033BDD", "#0D3EB5", "#052A85"],
+  };
+
   return (
-    <div className="sticky gap-11 flex flex-col items-center justify-center h-[400px]">
+    <div className="sticky md:gap-11  flex md:flex-col flex-col items-center justify-center md:h-[400px]">
       <div>
-        <h1 className="font-anton flex gap-2 text-6xl text-white">
-          Days I <b className=" text-blue-500"> Code</b>
+        <h1 className="font-anton md:gap-2 md:text-6xl my-11 flex text-4xl text-white">
+          Days I <b className="pl-2 text-blue-500"> Code</b>
         </h1>
       </div>
-      <div className="w-max flex text-white">
-        <ReactGitHubCalendar username="SuhasLingam" />
+      <div className="md:w-max flex text-white">
+        {
+          <ReactGitHubCalendar
+            username="SuhasLingam"
+            colorScheme="dark"
+            theme={explicitTheme}
+          />
+        }
       </div>
     </div>
   );
--- client/src/components/About/skills.jsx
+++ client/src/components/About/skills.jsx
@@ -28,90 +28,15 @@ import { SiVercel } from "react-icons/si";
 import sock from "../../assets/socket-io-icon.svg";
 
 const skills = () => {
-  const SkillIcons = [
-    {
-      name: <CPlain size={50} />,
-    },
-    {
-      name: <CplusplusOriginal size={50} />,
-    },
-    {
-      name: <PythonOriginalWordmark size={50} />,
-    },
-    {
-      name: <Html5Original size={50} />,
-    },
-    {
-      name: <Css3Original size={50} />,
-    },
-    {
-      name: <TailwindcssPlain size={50} />,
-    },
-    {
-      name: <JavascriptOriginal size={50} />,
-    },
-    {
-      name: <ReactOriginal size={50} />,
-    },
-    {
-      name: <SolidityOriginal size={50} />,
-    },
-    {
-      name: <BashOriginal size={50} />,
-    },
-    {
-      name: <VscodeOriginal size={50} />,
-    },
-    {
-      name: <Windows8Original size={50} />,
-    },
-    {
-      name: <AnacondaOriginal size={50} />,
-    },
-    {
-      name: <img src={sock} className="size-[60px]" />,
-    },
-    {
-      name: <DockerOriginalWordmark size={50} />,
-    },
-    {
-      name: <GitOriginal size={50} />,
-    },
-    {
-      name: <GithubOriginal size={50} />,
-    },
-    {
-      name: <SiVercel className="text-black" size={50} />,
-    },
-    {
-      name: <LinuxOriginal size={50} />,
-    },
-    {
-      name: <DebianOriginal size={50} />,
-    },
-    {
-      name: <MysqlOriginalWordmark size={50} />,
-    },
-    {
-      name: <SshOriginalWordmark size={50} />,
-    },
-    {
-      name: <UbuntuPlain size={50} />,
-    },
-    {
-      name: <img src={block} className="size-[70px]" />,
-    },
-  ];
-
   return (
-    <div className="md:flex-col md:items-center md:justify-center md:w-full md:h-screen sticky flex">
-      <h1 className="md:mb-[60px] md:text-6xl font-anton text-blue-500">
-        My Tech Stack
+    <div className="md:flex-col md:items-center md:justify-center md:w-full md:h-screen sticky flex flex-col items-center justify-center">
+      <h1 className="md:mb-[60px] md:text-6xl my-11 text-4xl font-anton text-white">
+        My <b className="text-blue-500">Tech</b> Stack
       </h1>
 
-      <div className="md:gap-9 md:h-fit md:grid-cols-6 md:grid-rows-4 grid grid-flow-row text-white">
+      <div className="md:gap-9 gap-7 md:h-fit md:grid-cols-6 md:grid-rows-4 grid-rows-8 grid grid-flow-row grid-cols-3 p-5 text-white">
         {SkillIcons.map((i) => (
-          <span className="md:w-[150px] rounded-2xl hover:transition-all hover:ease-in-out hover:delay-75 ring-2 hover:shadow-custom hover:shadow-blue-600 ring-white md:h-[90px] justify-center items-center flex md:p-4 md:text-3xl bg-blue-900 bg-opacity-[60%] ">
+          <span className="md:w-[150px] w-[120px] h-[85px] rounded-2xl hover:transition-all hover:ease-in-out hover:delay-0 ring-2 hover:shadow-custom hover:shadow-blue-600 ring-white md:h-[90px] justify-center items-center flex md:p-4 md:text-3xl bg-blue-900 bg-opacity-[60%] ">
             {i.name}
           </span>
         ))}
@@ -121,3 +46,78 @@ const skills = () => {
 };
 
 export default skills;
+
+const SkillIcons = [
+  {
+    name: <CPlain size={50} />,
+  },
+  {
+    name: <CplusplusOriginal size={50} />,
+  },
+  {
+    name: <PythonOriginalWordmark size={50} />,
+  },
+  {
+    name: <Html5Original size={50} />,
+  },
+  {
+    name: <Css3Original size={50} />,
+  },
+  {
+    name: <TailwindcssPlain size={50} />,
+  },
+  {
+    name: <JavascriptOriginal size={50} />,
+  },
+  {
+    name: <ReactOriginal size={50} />,
+  },
+  {
+    name: <SolidityOriginal size={50} />,
+  },
+  {
+    name: <BashOriginal size={50} />,
+  },
+  {
+    name: <VscodeOriginal size={50} />,
+  },
+  {
+    name: <Windows8Original size={50} />,
+  },
+  {
+    name: <AnacondaOriginal size={50} />,
+  },
+  {
+    name: <img src={sock} className="size-[60px]" />,
+  },
+  {
+    name: <DockerOriginalWordmark size={50} />,
+  },
+  {
+    name: <GitOriginal size={50} />,
+  },
+  {
+    name: <GithubOriginal size={50} />,
+  },
+  {
+    name: <SiVercel className="text-black" size={50} />,
+  },
+  {
+    name: <LinuxOriginal size={50} />,
+  },
+  {
+    name: <DebianOriginal size={50} />,
+  },
+  {
+    name: <MysqlOriginalWordmark size={50} />,
+  },
+  {
+    name: <SshOriginalWordmark size={50} />,
+  },
+  {
+    name: <UbuntuPlain size={50} />,
+  },
+  {
+    name: <img src={block} className="size-[70px]" />,
+  },
+];
