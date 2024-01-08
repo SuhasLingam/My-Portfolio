396b980cf63dd14ec3f148b2f6aecc3fb8d53fd6
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-01-08 14:36:18 +0530

    Made Callender Responsive :white_check_mark:

--- client/src/components/About/callender.jsx
+++ client/src/components/About/callender.jsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from "react";
 import ReactGitHubCalendar from "react-github-calendar";
 
-const callender = () => {
-  const explicitTheme = {
-    dark: ["#383838", "#2463FF", "#033BDD", "#0D3EB5", "#052A85"],
+const Calendar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const makeResponsive = () => {
+    setIsMobile(window.innerWidth <= 640);
   };
 
+  useEffect(() => {
+    makeResponsive();
+    window.addEventListener("resize", makeResponsive);
+    return () => {
+      window.removeEventListener("resize", makeResponsive);
+    };
+  }, []);
+
+  const explicitTheme = {
+    dark: ["#383838", "#5582FB", "#104EF2", "#0C40CB", "#0B35A5"],
+  };
+
+  const blockSize = isMobile ? 5 : 14;
+  const TextSize = isMobile ? 14 : 20;
+
   return (
-    <div className="sticky md:gap-11  flex md:flex-col flex-col items-center justify-center md:h-[400px]">
+    <div
+      className={`sticky md:gap-11 my-11 flex flex-col ${
+        isMobile ? "flex-col" : ""
+      } items-center justify-center md:h-[400px]`}
+    >
       <div>
         <h1 className="font-anton md:gap-2 md:text-6xl my-11 flex text-4xl text-white">
           Days I <b className="pl-2 text-blue-500"> Code</b>
         </h1>
       </div>
-      <div className="md:w-max flex text-white">
-        {
-          <ReactGitHubCalendar
-            username="SuhasLingam"
-            colorScheme="dark"
-            theme={explicitTheme}
-          />
-        }
+      <div className={`md:w-max flex ${isMobile ? "flex-col" : ""} text-white`}>
+        <ReactGitHubCalendar
+          username="SuhasLingam"
+          colorScheme="dark"
+          theme={explicitTheme}
+          blockSize={blockSize}
+          fontSize={TextSize}
+        />
       </div>
     </div>
   );
 };
 
-export default callender;
+export default Calendar;
