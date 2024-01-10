3943c23728f16fd3b55a2019cb154ae61a8cd571
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-01-10 19:49:27 +0530

    updated Porjects1.jsx

--- client/src/components/Projects/project1.jsx
+++ client/src/components/Projects/project1.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import recipeImage from "../../assets/recipe.png";
+import weatherImage from "../../assets/weather.png";
 
 const project1 = () => {
   const ProList = [
     {
       name: "Get Recipe App",
       link: "https://receipe-app-iota.vercel.app/",
+      pic: recipeImage,
     },
     {
-      name: "Get Recipe App",
-      link: "https://receipe-app-iota.vercel.app/",
+      name: "Weather App",
+      link: "https://weather-app-rose-omega.vercel.app/",
+      pic: weatherImage,
     },
     {
       name: "Get Recipe App",
@@ -51,8 +54,19 @@ const project1 = () => {
       <div className="flex items-center justify-center">
         <div className="md:gap-11 w-max md:grid-cols-4 gap-11 grid grid-cols-2">
           {ProList.map((items) => (
-            <span className="md:w-[340px] w-[160px] rounded-2xl backdrop-blur-[10px] hover:transition-all ring-4 ring-blue-500 hover:delay-75 hover:ease-in-out hover:shadow-blue-500 hover:shadow-custom md:flex-col  md:h-[450px] h-[240px] flex relative justify-center items-center">
-              <a href={items.link}>{items.name}</a>
+            <span className="md:w-[340px] w-[160px] rounded-2xl backdrop-blur-[10px] hover:transition-all ring-4 ring-opacity-50 ring-blue-500 hover:delay-75 hover:ease-in-out hover:shadow-blue-500 hover:shadow-custom md:flex-col  md:h-[450px] h-[240px] flex relative justify-center items-center">
+              <div className="rounded-t-2xl absolute w-full h-[70%] top-0">
+                <img
+                  src={items.pic}
+                  className="w-full rounded-t-2xl h-[100%]"
+                />
+                <div className="absolute w-full top-0 rounded-t-2xl h-[100%] hover:transition-all hover:ease-in-out  hover:bg-pink-300"></div>
+              </div>
+              <div className="absolute w-full h-[30%] items-center justify-center  flex rounded-b-2xl bottom-0 bg-transparent">
+                <a className="p-3 border-2 rounded-lg" href={items.link}>
+                  {items.name}
+                </a>
+              </div>
             </span>
           ))}
         </div>
